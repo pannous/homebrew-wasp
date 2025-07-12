d16650b9f80088e160d299b8455582b5073dbefb
pannous <info@pannous.com>
2025-07-12 17:07:59 +0200

    ok

--- Formula/wasp.rb
+++ Formula/wasp.rb
@@ -1,12 +1,25 @@
 class Wasp < Formula
   desc "Wasp binary"
   homepage "https://github.com/pannous/wasp"
-  url "file:///opt/homebrew/Library/Taps/pannous/homebrew-wasp/wasp"
+
+  homepage "https://github.com/pannous/wasp"
+  url "https://github.com/pannous/wasp.git", :using => :git, :tag => "v1.0.0"
   version "1.0.0"
-  
+
   def install
-    bin.install "wasp"
+    bin.install "bin/mac/wasp"
   end
+  
+  # url "file:///opt/homebrew/Library/Taps/pannous/homebrew-wasp/wasp"
+  # url "https://github.com/pannous/wasp/raw/main/bin/mac/wasp" # nightly
+  # url "https://github.com/pannous/wasp/releases/download/latest/wasp"
+  # url "https://github.com/pannous/wasp/releases/download/v1.0.0/wasp"
+  # sha256 "865cb4a222e50b2dd60a41e1e3e43426aba3880fafd550561c194440a42ef0d2"
+  # version "1.0.0"
+  
+  # def install
+  #   bin.install "wasp"
+  # end
   
   test do
     system "#{bin}/wasp", "--version"
--- README.md
+++ README.md
@@ -2,15 +2,15 @@
 
 ## How do I install these formulae?
 
-`brew install pannous/wasp/<formula>`
+`brew install pannous/wasp/wasp`
 
-Or `brew tap pannous/wasp` and then `brew install <formula>`.
+Or `brew tap pannous/wasp` and then `brew install wasp`.
 
 Or, in a `brew bundle` `Brewfile`:
 
 ```ruby
 tap "pannous/wasp"
-brew "<formula>"
+brew "wasp"
 ```
 
 ## Documentation
